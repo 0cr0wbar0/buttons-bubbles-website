cd9578e933316459b203dd122cd1c4e40a32d3d3
0cr0wbar0 <finnianholland2@gmail.com>
2026-02-09 11:13:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function clicked() {
+    let addition = document.createElement("p");
+    addition.innerText = "Button was clicked!";
+    document.querySelector("main").appendChild(addition);
+}
