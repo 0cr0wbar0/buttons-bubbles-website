a0059198b242d6f6a68ab85bd42340db137f5c0b
0cr0wbar0 <finnianholland2@gmail.com>
2026-02-19 14:23:53 +0000

    Initial React commit

--- script.js
+++
@@ -1,6 +0,0 @@
-
-function clicked() {
-    let addition = document.createElement("p");
-    addition.innerText = "Button was clicked!";
-    document.querySelector("main").appendChild(addition);
-}
